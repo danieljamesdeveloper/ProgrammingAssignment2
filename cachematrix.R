cb34b3af3c1a296f05f62a0f6e90ed951962483a
danieljamesdeveloper <66390723+danieljamesdeveloper@users.noreply.github.com>
2020-06-20 12:23:32 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+# Definesa the get and set methods, and get and set inverse methods. Creates the cache as a list.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+# Returns from the argument value from the cache if it exists, otherwise
+# re-calculates, sets the cache, and retruns the recalculated value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
